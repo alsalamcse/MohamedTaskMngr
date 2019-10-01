f55c2784e117ef45e4d9a599db0c5c322c271150
karawan <banat811@gmail.com>
2019-10-01 12:20:12 +0300

    30/9/2019

--- app/src/main/java/com/example/mohamedtaskmngr/addTask.java
+++ app/src/main/java/com/example/mohamedtaskmngr/addTask.java
@@ -8,6 +8,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.SeekBar;
 
+import com.example.mohamedtaskmngr.data.MyTask;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class addTask extends AppCompatActivity {
     private EditText etTitle;
     private EditText etSubject;
@@ -48,5 +52,19 @@ public class addTask extends AppCompatActivity {
             isok = false;
 
         }
+        MyTask t=new MyTask();
+        t.setSubject(subject);
+        t.setTitle(tittle);
+        creatMyTaskk(t);
+    }
+
+    private void creatMyTaskk(MyTask t) {                                    //save in data base ******important
+       //1.building object in data base
+        FirebaseDatabase database=FirebaseDatabase.getInstance();
+        //2.
+        DatabaseReference reference = database.getReference();
+        String key = reference.child("tasks").push().getKey();
+        reference.child("tasks").child(key).setValue(t);            ////bena2 awla
+
     }
 }
