48b01627d9e3bd42a4c0c2d01969f3e9d6810be1
karawan <banat811@gmail.com>
2019-10-08 12:20:48 +0300

    8/10/2019

--- app/src/main/java/com/example/mohamedtaskmngr/WelcomeScreen.java
+++ app/src/main/java/com/example/mohamedtaskmngr/WelcomeScreen.java
@@ -5,27 +5,46 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class WelcomeScreen extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome_screen);
-        Thread th=new Thread()
-        {
+    protected void onResume() {
+        super.onResume();
+        Thread th = new Thread() {
             @Override
             public void run() {
                 super.run();
                 try {
-                    sleep(3*1000);
-                    Intent i=new Intent(getApplication(),SignIn.class);
-                    startActivity(i);
-                } catch (InterruptedException e) {
+                    sleep(3 * 1000);
+                    FirebaseAuth auth = FirebaseAuth.getInstance();
+                    if (auth.getCurrentUser() == null || auth.getCurrentUser().getEmail() == null) {
+                        Intent i = new Intent(getApplication(), SignIn.class);
+                        startActivity(i);
+                        finish();
+                    } else {
+                        Intent i = new Intent(getApplication(), MainTasksActivity.class);
+                        startActivity(i);
+
+                    }
+
+                }catch(InterruptedException e){
                     e.printStackTrace();
                 }
             }
         };
         th.start();
 
+    }
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen);
     }
 }
