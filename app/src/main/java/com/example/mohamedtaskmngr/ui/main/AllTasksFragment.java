ba604449a6c64428b63a3dc88eeb0899afa30faf
karawan <banat811@gmail.com>
2019-10-27 12:19:49 +0200

    27/10/2019

---
+++ app/src/main/java/com/example/mohamedtaskmngr/ui/main/AllTasksFragment.java
@@ -0,0 +1,73 @@
+package com.example.mohamedtaskmngr.ui.main;
+
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.mohamedtaskmngr.R;
+import com.example.mohamedtaskmngr.data.MyTask;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AllTasksFragment extends Fragment {
+
+
+    public AllTasksFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_all_tasks, container, false);
+    }
+    public void  readTasksFromFirebase()
+    {
+        FirebaseDatabase database=FirebaseDatabase.getInstance();//CONNECT TO DATABASE
+        FirebaseAuth auth=FirebaseAuth.getInstance();//to get curret uid
+        String uid = auth.getUid();
+        DatabaseReference reference = database.getReference();
+        reference.child("tasks").child(uid).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot)//ae
+            {
+                for (DataSnapshot d:dataSnapshot.getChildren()) {
+                    MyTask t=d.getValue(MyTask.class);
+                    Log.d("MYTask",t.toString());
+
+                }
+
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+
+
+
+
+
+
+}
